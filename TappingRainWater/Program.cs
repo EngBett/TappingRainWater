6cba607e5f459046f34f07cc58386004b80f8bcb
Ronald Bett <rbett@jambopay.com>
2021-10-24 07:54:11 +0300

    Initial commit

---
+++ TappingRainWater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TappingRainWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
